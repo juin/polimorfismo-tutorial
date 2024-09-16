846613b3b0cbbca6ba4a22f32b0a7cd069e05057
Junior Lima <cremildolima@gmail.com>
2024-09-15 22:24:16 -0300

    relacionamento do resource users que exibe todos os locais que o usuário faz parte da coordenação.

---
+++ app/Filament/Resources/UserResource/RelationManagers/CoordenadorsRelationManager.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\RelationManagers;
+
+use App\Models\Estado;
+use App\Models\Pais;
+use Filament\Forms;
+use Filament\Forms\Components\MorphToSelect;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Models\Cidade;
+use Illuminate\Support\Facades\Log;
+
+class CoordenadorsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'coordenadores';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('cargo_id')
+            ->columns([
+                Tables\Columns\TextColumn::make('cargo.nome'),
+                Tables\Columns\TextColumn::make('coordenadorable.nome')
+                    ->label('Local Coordenador'),
+            ])
+            ->filters([
+                Tables\Filters\SelectFilter::make('cargo')
+                    ->relationship('cargo', 'nome')
+                    ->multiple(),
+                //Filtro usando o relacionamento polimorfico do modelo Coordenador.
+                Tables\Filters\Filter::make('local_coordenado')
+                    ->form([
+                        MorphToSelect::make('coordenadorable')
+                            ->label('Local Coordenado')
+                            ->types([
+                                MorphToSelect\Type::make(Pais::class)
+                                    ->titleAttribute('nome'),
+                                MorphToSelect\Type::make(Estado::class)
+                                    ->titleAttribute('nome'),
+                                MorphToSelect\Type::make(Cidade::class)
+                                    ->titleAttribute('nome')
+                            ]),
+                    ])->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['coordenadorable_type'],
+                                fn (Builder $query, $tipo): Builder => $query->where('coordenadorable_type', '=', $tipo)
+                            )
+                            ->when(
+                                $data['coordenadorable_id'],
+                                fn (Builder $query, $id): Builder => $query->where('coordenadorable_id', '=', $id)
+                            );
+                    }),
+                Tables\Filters\SelectFilter::make('coordenadorable_type')
+                    ->options([
+                        Cidade::class => 'Cidade',
+                        Estado::class => 'Estado',
+                        Pais::class => 'País',
+                    ]),
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
